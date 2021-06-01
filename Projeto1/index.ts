90c4de17ea464936850fb5b54eccea7888871863
Larisse Rodrigues <57159487+helloLari@users.noreply.github.com>
2021-06-01 15:31:05 -0300

    atualização de form e projeto

---
+++ Projeto1/index.ts
@@ -0,0 +1,4 @@
+function sum(n: number, n2: number){
+    return n + n2
+}
+console.log(sum(68, 100))
---
+++ angular-formulario/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { StyledInputComponent } from './styled-input/styled-input.component';
+import { StyledButtonComponent } from './styled-button/styled-button.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StyledInputComponent,
+    StyledButtonComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
